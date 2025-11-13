ad39ff1bc2f8a7b9c5eb3bb680123d36974d50d0
amjadnatout <na80@hotmail.com>
2025-11-13 18:02:22 +0100

    create attack class methods

--- PokemonGame/Attack.cs
+++ PokemonGame/Attack.cs
@@ -5,5 +5,21 @@
         public string Name { get; }
         public ElementType Type { get; }
         public int BasePower { get; }
+
+        public Attack(string name, ElementType type, int basePower)
+        {
+            ArgumentException.ThrowIfNullOrWhiteSpace(name);
+            if (basePower < 0)
+                throw new ArgumentException("Base power cannot be negative", nameof(basePower));
+
+            Name = name;
+            Type = type;
+            BasePower = basePower;
+        }
+        public void Use(int level)
+        {
+            int totalPower = BasePower + level;
+            Console.WriteLine($"{Name} hit with a total power of {totalPower}");
+        }
     }
 }
