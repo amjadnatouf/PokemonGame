48e7344f02ce76a98efcae46b8c5bb1489ee1c16
amjadnatout <na80@hotmail.com>
2025-11-13 17:46:00 +0100

    create attack class

---
+++ PokemonGame/Attack.cs
@@ -0,0 +1,9 @@
+﻿namespace PokemonGame
+{
+    internal class Attack
+    {
+        public string Name { get; }
+        public ElementType Type { get; }
+        public int BasePower { get; }
+    }
+}
