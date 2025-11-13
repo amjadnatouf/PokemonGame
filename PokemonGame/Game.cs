30967352c30f7062699f39830478b1244b80c8f6
amjadnatout <na80@hotmail.com>
2025-11-13 18:08:40 +0100

    create InitializeAttacks method in Game

--- PokemonGame/Game.cs
+++ PokemonGame/Game.cs
@@ -5,6 +5,22 @@ namespace PokemonGame
     {
         public Game()
         {
+            InitializeAttacks();
+        }
+
+        private void InitializeAttacks()
+        {
+            // Fire attacks
+            Attack flamethrower = new Attack("Flamethrower", ElementType.Fire, 12);
+            Attack ember = new Attack("Ember", ElementType.Fire, 6);
+
+            // Water attacks
+            Attack watergun = new Attack("Water Gun", ElementType.Water, 8);
+            Attack hydropump = new Attack("Hydro Pump", ElementType.Water, 15);
+
+            // Grass attacks
+            Attack vinewhip = new Attack("Vine Whip", ElementType.Grass, 7);
+            Attack razorleaf = new Attack("Razor Leaf", ElementType.Grass, 11);
         }
 
         internal void start()
