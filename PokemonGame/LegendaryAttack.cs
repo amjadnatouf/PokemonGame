898f1069e179c2e6ff2a09e2c35cb4ceb1f33cf9
amjadnatout <na80@hotmail.com>
2025-11-14 13:37:14 +0100

    create LegendaryAttack class

---
+++ PokemonGame/LegendaryAttack.cs
@@ -0,0 +1,16 @@
+﻿namespace PokemonGame
+{
+    internal class LegendaryAttack : Attack
+    {
+        public LegendaryAttack(Attack baseAttack)
+            : base(baseAttack.Name, baseAttack.Type, baseAttack.BasePower)
+        {
+        }
+
+        public override void Use(int level)
+        {
+            int totalPower = BasePower + level * 2;
+            Console.WriteLine($"{Name} unleashes its potential with total power {totalPower}!");
+        }
+    }
+}
